c0da3b3e8304a7a001a51c74faa914584a47686f
Romain <romain.mard@gmail.com>
2021-06-22 22:59:49 +0200

    feat: update tailwind template (#9)

---
+++ src/views/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer justify-end h-10 bg-primary flex">
+      <a href="http://localhost" className="text-primary p-2">
+        Privacy policy
+      </a>
+      <a href="http://localhost" className="text-primary p-2">
+        About us
+      </a>
+      <a
+        href="https://github.com/CenturioHackMoney21"
+        className="text-primary p-2"
+      >
+        Copyright
+      </a>
+    </footer>
+  );
+}
--- src/views/OnBoarding/OnBoardingView.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function OnBoardingView() {
-  return <div>Onboarding</div>;
-}
