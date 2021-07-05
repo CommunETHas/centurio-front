23d718a51b2a05427d890c6ffb32fb78cfb7a799
croquelois <croquelois.adrien@gmail.com>
2021-07-05 23:57:37 +0200

    fix: heroku routing

--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -1,15 +1,28 @@
 import React, { ReactElement } from 'react';
+import { useHistory } from 'react-router-dom';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Footer(): ReactElement {
+  const history = useHistory();
+  const redirectAboutUs = (path: string) => {
+    history.push(path);
+  };
   return (
     <footer className="footer justify-end h-10 bg-primary flex items-center">
-      <a href="./privacy-policy" className="text-secondary p-2">
+      <button
+        type="button"
+        onClick={() => history.push('/privacy-policy')}
+        className="text-secondary p-2"
+      >
         Privacy policy
-      </a>
-      <a href="./about-us" className="text-secondary p-2">
+      </button>
+      <button
+        type="button"
+        onClick={() => history.push('/about-us')}
+        className="text-secondary p-2"
+      >
         About us
-      </a>
+      </button>
       <a
         href="https://github.com/CenturioHackMoney21"
         className="text-secondary ml-2 mr-4"
